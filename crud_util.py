389d9de38cccf9713f043857fb3318a3dc2ed1c4
Feliphe B <feliphezion@gmail.com>
2016-07-14 01:36:49 -0300

    crud_util module: added transaction to CRUD operations.
    Signed-off-by: Feliphe B. <feliphezion@gmail.com>

--- crud_util.py
+++ crud_util.py
@@ -9,10 +9,31 @@ class CrudUtil():
         self.con = Conexao(link)
 
     def insert(self, tabela, camposValores):
-        return self.con.qb().table(tabela).insert_get_id(camposValores)
+        try:
+            self.con.qb().begin_transaction()
+            id = self.con.qb().table(tabela).insert_get_id(camposValores)
+            self.con.qb().commit()
+            return id
+        except Exception as e:
+            self.con.qb().rollback()
+            raise Exception("Database transaction failed. Details: "+ str(e))
 
     def update(self, tabela, where, camposValores):
-        return self.con.qb().table(tabela).where(where).update(camposValores)
+        try:
+            self.con.qb().begin_transaction()
+            id = self.con.qb().table(tabela).where(where).update(camposValores)
+            self.con.qb().commit()
+            return id
+        except Exception as e:
+            self.con.qb().rollback()
+            raise Exception("Database transaction failed. Details: " + str(e))
 
     def delete(self, tabela, where):
-        return self.con.qb().table(tabela).where(where).delete()
+        try:
+            self.con.qb().begin_transaction()
+            id = self.con.qb().table(tabela).where(where).delete()
+            self.con.qb().commit()
+            return id
+        except Exception as e:
+            self.con.qb().rollback()
+            raise Exception("Database transaction failed. Details: " + str(e))
