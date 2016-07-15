ab528cfafc64029ef04970f158e9d026f8f3f40c
Feliphe B <feliphezion@gmail.com>
2016-07-15 04:24:29 -0300

    Final improvements, ready for production.

--- processa_evento.py
+++ processa_evento.py
@@ -12,10 +12,10 @@ class processaEvento:
 
     #Processa eventos no repositorio no Github
     def processaEvento(self, evento):
+        dados = self.data
 
         #Novo Pull Request
         if evento == 'pull_request':
-            dados = self.data
             dadosPullRequest = self.processaDados.getDadosPullRequest(dados)
 
             if 'action' in dados and dados['action'] == 'opened' and 'id' in dadosPullRequest:
@@ -40,13 +40,35 @@ class processaEvento:
 
                 for usuarioCod in usuarios:
                     self.processaDados.enviaNotificacao(usuarioCod, titulo, descricao, warnLevel, link)
+
+                return "OK"
             else:
                 raise Exception("Payload incompleto ou em formato incorreto.")
 
         # Novo Push
         elif evento == 'push':
-            ""
+            repositorio = self.processaDados.getDadosRepo(dados['repository'])
+            branches    = self.processaDados.getBranches(repositorio['repositorioCod'], dados['repository']['branches_url'][0:-9])
+            usuarios    = {1}
+
+            head = dados['head_commit']
+
+            data = head['timestamp'][0:10]
+            hora = head['timestamp'][11:-4]
+
+            titulo = 'Novo Push no repositorio '+ repositorio['repositorioNome']
+            descricao = 'Ultimo commit no branch '+ dados['ref']  +',<br /> por <strong>'+ head['author']['name'] +'</strong>, \
+                         em <strong>'+ data +'</strong>, as <strong>'+ hora +'</strong>Arquivos adicionados: <strong>'+ \
+                        str(len(head['added'])) +'</strong>. Removidos: <strong>'+ str(len(head['removed'])) +'</strong>. \
+                        Alterados: <strong>'+ str(len(head['modified'])) +'</strong>';
+
+            warnLevel   = 'warning';
+            link        = head['url'];
+
+            for usuarioCod in usuarios:
+                self.processaDados.enviaNotificacao(usuarioCod, titulo, descricao, warnLevel, link)
+
+            return "OK"
         else:
-            ""
-        return
+            return "Evento desconhecido"
 
