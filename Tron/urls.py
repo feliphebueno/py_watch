269e5e2d9f7d760cbbbad1997b159661d2e12cc3
Rinzler <feliphezion@gmail.com>
2016-09-01 21:47:02 -0300

    remoo de lixo
    Signed-off-by: Rinzler <feliphezion@gmail.com>

--- Tron/urls.py
+++ Tron/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+import django.contrib.auth
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('CLU.urls')),
-
+django.contrib.auth.views.login
 ]
