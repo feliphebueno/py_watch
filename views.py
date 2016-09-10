676e0734306bd264cb667f099b2075993fc7b149
Bueno, Feliphe <feliphezion@gmail.com>
2016-09-10 20:54:21 -0300

    Multi-db config

--- views.py
+++ views.py
@@ -8,9 +8,9 @@ def index(request):
     return render(request, 'index.html', {})
 
 def getUsers(request):
-    users = User.objects.all()
+    users = User.objects.using('chat_watch').all()
     return render(request, 'users.html', {'users': users})
 
 def userHistory(request, userId):
-    history = Message.objects.filter(userid=userId)
+    history = Message.objects.using('chat_watch').filter(userid=userId)
     return render(request, 'userHistory.html', {'history': history})
