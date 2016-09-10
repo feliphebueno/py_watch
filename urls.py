5dc4859e23bb9571a1b0a51e95200c01d7436baa
Bueno, Feliphe <feliphezion@gmail.com>
2016-09-10 20:38:13 -0300

    Admin config

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^admin/', admin.site.urls),
     url(r'^users/?$', views.getUsers),
     url(r'^userHistory/([0-9]{1,})/?$', views.userHistory),
 ]
